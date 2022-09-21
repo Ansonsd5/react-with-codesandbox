a67aafec1315090084c65313f1aff00fa357247c
Anson Saver Dsouza <85802445+Ansonsd5@users.noreply.github.com>
2022-09-21 09:32:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1> Data </h1>
+      <button onClick> Click to load data from server </button>
+    </div>
+  );
+}
